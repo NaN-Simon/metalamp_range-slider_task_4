9a97747cdbfa9b8c7f4b9980b6bd488f707af3a1
NaN-Simon <goosaimon@gmail.com>
2022-11-27 20:09:16 +0300

    refactor: change folders, feat: add screenboard component

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import './styles/_fonts.scss';
 import './styles/_index.scss';
 
 import './slider/slider.ts';
+import './screenboard/screenboard.ts';
--- src/slider/index.ts
+++
@@ -1,6 +0,0 @@
-import './styles/_reset.scss';
-import './styles/_fonts.scss';
-import './styles/_index.scss';
-
-import './slider/slider.ts';
-import './screenboard/screenboard.ts';
