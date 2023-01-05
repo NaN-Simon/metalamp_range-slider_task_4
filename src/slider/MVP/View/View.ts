e12e9d84f4b0f857d420fdd12911a9fda9ef6d8d
NaN-Simon <goosaimon@gmail.com>
2023-01-05 10:40:08 +0300

    refactor: clean extra-info; feat: add step for thumbTo

--- src/slider/MVP/View/View.ts
+++ src/slider/MVP/View/View.ts
@@ -21,8 +21,6 @@ export default class View extends Observer<IViewValue> {
     this.wrapperElement.classList.add('range-slider-wrapper')
     this.initComponents();
     this.subscribeThumbs();
-    // console.log(this.config);
-
   }
 
   updateConfig(value: IConfig): void {
@@ -37,7 +35,7 @@ export default class View extends Observer<IViewValue> {
   initComponents() {
     this.progressBar = new ProgressBar(this.wrapperElement);
     this.thumbFrom = new Thumb(this.wrapperElement, 'from');
-    // this.thumbTo = new Thumb(this.wrapperElement, 'to');
+    this.thumbTo = new Thumb(this.wrapperElement, 'to');
     this.scale = new Scale(this.wrapperElement, defaultConfig);
   }
 
@@ -45,55 +43,41 @@ export default class View extends Observer<IViewValue> {
   subscribeThumbs(){
     console.log('запуск subscribeThumbs во View');
     this.thumbFrom.subscribe((data) => {
-      /* test */
-      const thumbFromOffsetWTest = this.thumbFrom.getThumb.offsetWidth
       const separatorCounts = Math.ceil((defaultConfig.max - defaultConfig.min)/ defaultConfig.step)
-      // console.log(separatorCounts);
-      const pixelStep = ((data.rect.width/separatorCounts)) /* - thumbFromOffsetWTest/2 */
-                  // console.log(pixelStep, pixelStep*separatorCounts);
-
-
-      /* INFO TEST */
-      let showInner = 'Ключевые точки:'
-      for(let i = 0; i <= separatorCounts; i++){
-        showInner += `<p>${i+1}  -  ${pixelStep*i}</p>`
-      }
-      document.querySelector('.v5')!.innerHTML = showInner
-
-
-
-      /* test */
-
+      const pixelStep = ((data.rect.width/separatorCounts))
       const rectX = data.rect.x
-      document.querySelector('.v3')!.innerHTML = `${rectX} - rectX`
       const rectWidth = data.rect.width
-      document.querySelector('.v4')!.innerHTML = `${rectWidth} - rectWidth`
       const thumbFromOffsetW = this.thumbFrom.getThumb.offsetWidth
 
       const shift = (Math.min(Math.max(0, data.position - rectX), rectWidth) / rectWidth) * rectWidth
-      this.thumbFrom.getThumb.style.left = `${shift /* - thumbFromOffsetW / 2 */}px`
-      document.querySelector('.v1')!.innerHTML = `${shift /* - thumbFromOffsetW / 2 */}px`
-      // console.log(shift);
 
       for(let i = 0; i <= separatorCounts; i++){
         if(shift >= (pixelStep * i) - thumbFromOffsetW / 2) {
-          document.querySelector('.v2')!.innerHTML = `i*pixelStep - ${(i*pixelStep).toFixed(1).toString()}`
-          this.thumbFrom.getThumb.style.left = `${i*pixelStep /* - thumbFromOffsetW / 2 */}px`
+          this.thumbFrom.getThumb.style.left = `${i * pixelStep}px`
         }
-
       }
     })
 
     if(!this.thumbTo){ return }
+
     this.thumbTo.subscribe((data) => {
+      const separatorCounts = Math.ceil((defaultConfig.max - defaultConfig.min)/ defaultConfig.step)
+      const pixelStep = ((data.rect.width/separatorCounts))
       const rectX = data.rect.x
       const rectWidth = data.rect.width
       const thumbToOffsetW = this.thumbTo.getThumb.offsetWidth
 
       const shift = (Math.min(Math.max(0, data.position - rectX), rectWidth) / rectWidth) * rectWidth
-      this.thumbTo.getThumb.style.left = `${shift - thumbToOffsetW / 2}px`
+
+      for(let i = 0; i <= separatorCounts; i++){
+        if(shift >= (pixelStep * i) - thumbToOffsetW / 2) {
+          this.thumbTo.getThumb.style.left = `${i * pixelStep}px`
+        }
+      }
     })
   }
+
+
 
   setConfig(value:IConfig){
     console.log('запуск setConfig во View');
